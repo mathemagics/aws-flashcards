bde6418524fbf1782942d19a98d5f061bac8a6a9
Paul Bodnar <higgsbison@gmail.com>
2024-04-23 11:55:15 -0600

    add cert request

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
 import {AwsService} from "~/service/aws/aws";
 
 export default async function HomePage() {
   const awsService =  new AwsService();
-  const flashcardItem = await awsService.getFlashcardItem({key: "developer-associate"});
-  console.log(flashcardItem);
+  const certs = await awsService.getCerts();
+  console.log('cert: ', certs);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#196f9a] to-[#05060c] text-white">
@@ -11,6 +12,12 @@ export default async function HomePage() {
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] text-center">
           AWS Flash Cards
         </h1>
+        {certs.map((cert) => (
+          <div key={cert.cert_id} className="flex flex-col items-center justify-center gap-4">
+            <h2 className="text-3xl font-bold">{cert.cert_id}</h2>
+            <Image src={cert.image} alt={`${cert.cert_id} certification logo`}/>
+          </div>
+        ))}
       </div>
     </main>
   );
--- src/service/aws/aws.ts
+++ src/service/aws/aws.ts
@@ -2,30 +2,78 @@ type MethodType = "GET" | "POST" | "PUT" | "DELETE";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type AnyType = any;
 
+type Cert = {
+  cert_id: string;
+  image: string;
+};
+
 export class AwsService {
   private async makeFetchRequest({method, path}: {method: MethodType, path: string}) {
     const awsURL = process.env.AWS_GATEWAY_ENDPOINT;
+
     if (!awsURL) {
       throw new Error("AWS_GATEWAY_ENDPOINT not set");
     }
-    return await fetch(
-      `${awsURL}${path ? `/${path}` : ""}`,
-      {
-        method,
-        headers: {
-          "Content-Type": "application/json",
+
+    try {
+      const res = await fetch(
+        `${awsURL}${path ? `/${path}` : ""}`,
+        {
+          method,
+          headers: {
+            "Content-Type": "application/json",
+          },
         },
-      },
-    );
+      );
+      if (!res.status.toString().startsWith("2")) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+      return res;
+    } catch (error) {
+      console.error('error: ', error);
+      throw error;
+    }
   }
 
   public async getFlashcardItem({key}: {key: string}) {
     'use server';
 
-    const res = await this.makeFetchRequest({
+    const prefix = "flashcards";
+
+    const response = await this.makeFetchRequest({
       method: "GET",
-      path: key,
+      path: `${prefix}/${key}`,
     });
-    return res.body;
+
+    const buffer = await response.arrayBuffer();
+    const uint8Array = new Uint8Array(buffer);
+    const textDecoder = new TextDecoder();
+    const jsonStr = textDecoder.decode(uint8Array);
+
+    // TODO FIX
+    return JSON.parse(jsonStr) as string[] || [];
+  }
+
+  public async getCerts(): Promise<Cert[]> {
+    'use server';
+
+    const prefix = "certs";
+
+    try {
+      const response = await this.makeFetchRequest({
+        method: "GET",
+        path: prefix,
+      })
+      const buffer = await response.arrayBuffer();
+      const uint8Array = new Uint8Array(buffer);
+      const textDecoder = new TextDecoder();
+      const jsonStr = textDecoder.decode(uint8Array);
+
+      return JSON.parse(jsonStr) as Cert[] || [];
+
+    } catch (error) {
+      console.error('error: ', error);
+      return [];
+    }
   }
 }
